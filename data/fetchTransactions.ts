999dcad5137d7a3573e4f1d454823a7d9bb1aeeb
Artur Wdowiarski <arturwdowiarski@me.com>
2022-04-23 10:40:16 +0200

    Cleaned transaction fetching code a bit

--- data/fetchTransactions.ts
+++ data/fetchTransactions.ts
@@ -8,19 +8,15 @@ export async function fetchTransactions(
         // TODO: Add support for chains other than ethereum
         throw new Error("Only Ethereum Network is currently supported")
     }
-
-    const response = await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=0x7043FE1d5cC719E7d242562A13C858036F48C997&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=PVGHK1NRJ1AMDH73RNW8HH95YUNE2EGH6Q`)
-
+    const apiEndpoint = 'https://api.etherscan.io/api'
+    const apiModule = 'account'
+    const apiAction = 'txList'
+    const apiKey = 'PVGHK1NRJ1AMDH73RNW8HH95YUNE2EGH6Q'
+    const response = await fetch(`${apiEndpoint}?module=${apiModule}&action=${apiAction}&address=${account}&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey=${apiKey}`)
     const data = await response.json()
-
-    // if (typeof data.result === 'string' || data.result instanceof String) {
-    //     return []
-    // }
-
     if (data.result === 'Max rate limit reached, please use API Key for higher rate limit') {
         // TODO: Add error handling that makes sense
         return []
     }
-
     return data.result
 }
