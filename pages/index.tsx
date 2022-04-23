c832358f71e206a1a853600425bb27ac86528f91
Jarek S <jarek.sygitowicz@authologic.com>
2022-04-23 13:10:20 +0200

    svg name fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -145,23 +145,23 @@ const Home: NextPage = () => {
     <div className="w-full p-2 md:w-1/3">
       <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
-        <line x1="128" y1="72" x2="128" y2="88" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="128" y1="168" x2="128" y2="184" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <circle cx="128" cy="128" r="96" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></circle>
-        <path d="M104,168h36a20,20,0,0,0,0-40H116a20,20,0,0,1,0-40h36" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+        <line x1="128" y1="72" x2="128" y2="88" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="128" y1="168" x2="128" y2="184" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <circle cx="128" cy="128" r="96" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></circle>
+        <path d="M104,168h36a20,20,0,0,0,0-40H116a20,20,0,0,1,0-40h36" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path>
       </svg>
       Automatic purchase price tracking of any crypto currency
     </div>
     <div className="w-full p-2">
       <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
-        <rect x="32" y="48" width="192" height="136" rx="8" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></rect>
-        <line x1="160" y1="184" x2="192" y2="224" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="96" y1="184" x2="64" y2="224" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="96" y1="120" x2="96" y2="144" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="128" y1="104" x2="128" y2="144" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="160" y1="88" x2="160" y2="144" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-        <line x1="128" y1="48" x2="128" y2="24" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
+        <rect x="32" y="48" width="192" height="136" rx="8" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></rect>
+        <line x1="160" y1="184" x2="192" y2="224" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="96" y1="184" x2="64" y2="224" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="96" y1="120" x2="96" y2="144" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="128" y1="104" x2="128" y2="144" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="160" y1="88" x2="160" y2="144" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
+        <line x1="128" y1="48" x2="128" y2="24" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line>
       </svg>
       Stablecoin yield tracking &<br />
       protocol suggestions
@@ -169,8 +169,8 @@ const Home: NextPage = () => {
     <div className="w-full p-2 md:w-1/3">
       <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
-        <path d="M122.3,71.4l19.8-19.8a44.1,44.1,0,0,1,62.3,62.3l-28.3,28.2a43.9,43.9,0,0,1-62.2,0" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
-        <path d="M133.7,184.6l-19.8,19.8a44.1,44.1,0,0,1-62.3-62.3l28.3-28.2a43.9,43.9,0,0,1,62.2,0" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+        <path d="M122.3,71.4l19.8-19.8a44.1,44.1,0,0,1,62.3,62.3l-28.3,28.2a43.9,43.9,0,0,1-62.2,0" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path>
+        <path d="M133.7,184.6l-19.8,19.8a44.1,44.1,0,0,1-62.3-62.3l28.3-28.2a43.9,43.9,0,0,1,62.2,0" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path>
       </svg>
       Multichain support. Polygon & Ethereum, more coming soon.
     </div>
