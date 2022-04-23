19a57edf9c1c04e9c1e4382320ef22bd18012a6e
Jarek S <jarek.sygitowicz@authologic.com>
2022-04-23 13:08:11 +0200

    class name fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,7 @@ const Home: NextPage = () => {
           <div className="w-full bg-gradient-to-br from-slate-100 to-slate-200 mt-10 p-6 font-dmsans tracking-tight text-slate-600">
   <div className="p-6 text-center md:flex">
     <div className="w-full p-2 md:w-1/3">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
+      <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
         <line x1="128" y1="72" x2="128" y2="88" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
         <line x1="128" y1="168" x2="128" y2="184" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
@@ -153,7 +153,7 @@ const Home: NextPage = () => {
       Automatic purchase price tracking of any crypto currency
     </div>
     <div className="w-full p-2">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
+      <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
         <rect x="32" y="48" width="192" height="136" rx="8" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></rect>
         <line x1="160" y1="184" x2="192" y2="224" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
@@ -167,7 +167,7 @@ const Home: NextPage = () => {
       protocol suggestions
     </div>
     <div className="w-full p-2 md:w-1/3">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
+      <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-10 w-10" fill="#000000" viewBox="0 0 256 256">
         <rect width="256" height="256" fill="none"></rect>
         <path d="M122.3,71.4l19.8-19.8a44.1,44.1,0,0,1,62.3,62.3l-28.3,28.2a43.9,43.9,0,0,1-62.2,0" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
         <path d="M133.7,184.6l-19.8,19.8a44.1,44.1,0,0,1-62.3-62.3l28.3-28.2a43.9,43.9,0,0,1,62.2,0" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
