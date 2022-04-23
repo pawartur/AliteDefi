034bb2d78248e60b164b6dfd79ecccf0db67dca9
Jarek S <jarek.sygitowicz@authologic.com>
2022-04-23 11:24:16 +0200

    added and configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "navy": "#272D73",
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+        "dmsans": ['DM Sans', 'sans-serif'],
+        "novesquare": ['Nova Square', 'sans-serif'],
+        "actor": ['Actor', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
