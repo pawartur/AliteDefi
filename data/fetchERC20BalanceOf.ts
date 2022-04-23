1a41bbd19b250f673b1f3d44f41a90bdd76b0484
Artur Wdowiarski <arturwdowiarski@me.com>
2022-04-23 19:12:14 +0200

    added fetchERC20BalanceOf function

---
+++ data/fetchERC20BalanceOf.ts
@@ -0,0 +1,12 @@
+import erc20abiJson from './erc20.abi.json'
+import { ethers } from 'ethers'
+
+export async function fetchERC20BalanceOf(
+    account: string,
+    provider: ethers.providers.Web3Provider,
+    smartContractAddress: string
+) : Promise<number> {
+    const contract = new ethers.Contract(smartContractAddress, erc20abiJson, provider);
+    const balance = (await contract.balanceOf(account)).valueOf();
+    return Number(balance)
+}
--- utils/ConnectionContext.ts
+++ utils/ConnectionContext.ts
@@ -1,10 +1,12 @@
 import React from "react"
+import { ethers } from "ethers"
 
 interface IConnectionContext {
     account: string | undefined;
     chainId: Number | undefined;
+    library: ethers.providers.Web3Provider | undefined;
 }
 
-const ConnectionContext = React.createContext<IConnectionContext>({account: undefined, chainId: undefined})
+const ConnectionContext = React.createContext<IConnectionContext>({account: undefined, chainId: undefined, library: undefined})
 
 export default ConnectionContext
