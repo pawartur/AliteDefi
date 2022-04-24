02f283bec8fda09a9a99eae421552f8acde1061f
Jarek S <jarek.sygitowicz@authologic.com>
2022-04-24 03:36:21 +0200

    changed bg for coins

--- components/CoinInfo.tsx
+++ components/CoinInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CoinInfo = (props) => {
-    return <div className="rounded-md border p-2">
+    return <div className="rounded-md border bg-green-200 p-2 ring-2 ring-green-700 ring-offset-2">
         <div className="text-sm">{props.symbol}</div>
         <div className="font-actor font-bold truncate text-lg">${Number((Number(props.amount) / Math.pow(10, props.decimal)) * parseFloat(props.priceInUSD)).toFixed(3)}</div>
     </div>
